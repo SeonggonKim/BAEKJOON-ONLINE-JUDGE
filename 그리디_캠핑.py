60ea394fe966f7491c97865f1be0e3b5ce8083a1
Seonggon Kim <goodk96@naver.com>
2021-03-17 09:10:44 +0900

    Add files via upload

---
+++ 그리디_캠핑.py
@@ -0,0 +1,28 @@
+import sys
+case = []
+
+while True:
+    case.append(list(map(int, sys.stdin.readline().split())))
+    if case[-1] == [0, 0, 0]:
+        break
+
+result = []
+print(case)
+for i in range(len(case)-1):
+    start_point = 0
+    count = 0
+    while True:
+        start_point += case[i][1]
+        if start_point > case[i][2] and (case[i][2] % case[i][1] < case[i][0]):
+            count += (case[i][2] % case[i][1])
+            result.append(count)
+            break
+        elif start_point > case[i][2] and (case[i][2] % case[i][1] >= case[i][0]):
+            count += case[i][0]
+            result.append(count)
+            break
+        else:
+            count += case[i][0]
+
+for i in range(len(result)):
+    print('Case ' + str(i+1) + ': ' + str(result[i]))
