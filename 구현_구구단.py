ecd4d0b068664e22aa5ce1f3cf87f5f0598c8c11
Seonggon Kim <goodk96@naver.com>
2021-06-08 17:39:55 +0900

    Rename 구현_구구단.py to 알고리즘 분류별/구현/구현_구구단.py

--- 구현_구구단.py
+++
@@ -1,4 +0,0 @@
-number = int(input())
-
-for i in range(1, 10):
-    print(str(number) + " * " + str(i) + " = " + str(number*i))
---
+++ 알고리즘 분류별/구현/구현_구구단.py
@@ -0,0 +1,4 @@
+number = int(input())
+
+for i in range(1, 10):
+    print(str(number) + " * " + str(i) + " = " + str(number*i))
