b66e163a2098153331c4d8024d34048b097666a2
Seonggon Kim <goodk96@naver.com>
2021-07-01 16:45:46 +0900

    Add files via upload

---
+++ 단계별/1단계 입출력과 사칙연산/곱셈.py
@@ -0,0 +1,12 @@
+A = input()
+B = input()
+
+three = int(A)*int(B[2])
+four = int(A)*int(B[1])
+five = int(A)*int(B[0])
+six = three + four * 10 + five * 100
+
+print(three)
+print(four)
+print(five)
+print(six)
